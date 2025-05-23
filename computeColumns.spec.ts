f00440b03d51c5fa1a720b7a2bc5ed75310bd724
Bernard Lambeau <bernard.lambeau@enspirit.be>
2025-05-23 11:25:16 +0200

    TDD - Step 6 - Show empty columns.

--- computeColumns.spec.ts
+++ computeColumns.spec.ts
@@ -63,4 +63,16 @@ describe('computeColumns', () => {
     ]);
   })
 
+  it('works on a pair on non consecutive months, while showing empty columns', () => {
+    const result = computeColumns([
+      { publication_date: date('2025-01-17') },
+      { publication_date: date('2025-03-22') },
+    ], false);
+    expect(result).to.eql([
+      date('2025-01-01'),
+      date('2025-02-01'),
+      date('2025-03-01'),
+    ]);
+  })
+
 })
--- computeColumns.ts
+++ computeColumns.ts
@@ -4,9 +4,20 @@ export const computeColumns = (cards, hide_empty_columns = false) => {
   const months = cards.map(c => c.publication_date.startOf('month')).sort();
   const min = months[0];
   const max = months[months.length-1];
-  if (min.equals(max)) {
-    return [min];
+
+  if (hide_empty_columns) {
+    if (min.equals(max)) {
+      return [min];
+    } else {
+      return [min, max];
+    }
   } else {
-    return [min, max];
+    const result = [];
+    let current = min;
+    while (current <= max) {
+      result.push(current);
+      current = current.plus({ month: 1 })
+    }
+    return result;
   }
 }
